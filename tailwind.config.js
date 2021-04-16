e085c43f0fe60e4130bb1044de917f13c0c15363
babs20 <thevillemc@gmail.com>
2021-04-16 17:59:39 -0400

    edit tailwind config to include correct color and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        gray: colors.gray,
+      },
+    },
   },
   variants: {
     extend: {},
