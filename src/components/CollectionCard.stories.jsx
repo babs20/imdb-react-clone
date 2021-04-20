a4590ef9c204d9dfaa7cde56e7bb1da590f515a8
babs20 <thevillemc@gmail.com>
2021-04-20 01:08:00 -0400

    add collection card

---
+++ src/components/CollectionCard.stories.jsx
@@ -0,0 +1,24 @@
+import CollectionCard from './CollectionCard';
+import '../styles/index.css';
+
+export default {
+  component: CollectionCard,
+  title: 'CollectionCard',
+  decorators: [
+    story => (
+      <section className='w-full h-full p-16 bg-gray-800'>{story()}</section>
+    ),
+  ],
+};
+
+const Template = args => <CollectionCard {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  collection: {
+    name: 'Star Wars Collection',
+    imageURL:
+      'https://image.tmdb.org/t/p/original/d8duYyyC9J5T825Hg7grmaabfxQ.jpg',
+    partTotal: '9',
+  },
+};
