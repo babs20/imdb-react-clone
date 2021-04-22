64c44319527536302c54567afcb3c5405cadf49a
babs20 <thevillemc@gmail.com>
2021-04-22 11:17:10 -0400

    change mock data

--- src/pages/Home.stories.jsx
+++ src/pages/Home.stories.jsx
@@ -36,45 +36,45 @@ Default.args = {
       genres: 'Animation • Sci-Fi & Fantasy • Drama',
     },
     {
-      name: 'Violet Evergarden',
+      name: 'A Place Further Than the Universe',
       imageURL:
-        'https://image.tmdb.org/t/p/original/ImvHbM4GsJJykarnOzhtpG6ax6.jpg',
-      rating: '8.7',
+        'https://image.tmdb.org/t/p/original/iERXyBf0K6DfAQ8oKEYrdjf1vIs.jpg',
+      rating: '7.7',
       genres: 'Animation • Sci-Fi & Fantasy • Drama',
     },
     {
-      name: '5 Centimeters per Second',
+      name: 'Asobi Asobase - workshop of fun -',
       imageURL:
-        'https://image.tmdb.org/t/p/original/t0ISGhwZtghakg46TWCOWBTIflZ.jpg',
-      rating: '7.3',
+        'https://image.tmdb.org/t/p/original/rAZRl8qvI1OldL2Smnu0MCMVzqP.jpg',
+      rating: '8.8',
       genres: 'Animation • Sci-Fi & Fantasy • Drama',
     },
     {
-      name: 'Ergo Proxy',
+      name: 'Land of the Lustrous',
       imageURL:
-        'https://image.tmdb.org/t/p/original/wXaWK1dtA5EACL3ASDKGKBJePsS.jpg',
-      rating: '7.5',
+        'https://image.tmdb.org/t/p/original/pUy4Dcb6UstCI64AYqLbdPISkDS.jpg',
+      rating: '7.8',
       genres: 'Animation • Sci-Fi & Fantasy • Drama',
     },
     {
-      name: 'Violet Evergarden',
+      name: 'A Silent Voice: The Movie',
       imageURL:
-        'https://image.tmdb.org/t/p/original/ImvHbM4GsJJykarnOzhtpG6ax6.jpg',
-      rating: '8.7',
+        'https://image.tmdb.org/t/p/original/tuFaWiqX0TXoWu7DGNcmX3UW7sT.jpg',
+      rating: '8.4',
       genres: 'Animation • Sci-Fi & Fantasy • Drama',
     },
     {
-      name: '5 Centimeters per Second',
+      name: 'Akira',
       imageURL:
-        'https://image.tmdb.org/t/p/original/t0ISGhwZtghakg46TWCOWBTIflZ.jpg',
-      rating: '7.3',
+        'https://image.tmdb.org/t/p/original/5KlRFKKSbyCiyYpZSS3A6G5bW0K.jpg',
+      rating: '7.9',
       genres: 'Animation • Sci-Fi & Fantasy • Drama',
     },
     {
-      name: 'Ergo Proxy',
+      name: 'Monogatari',
       imageURL:
-        'https://image.tmdb.org/t/p/original/wXaWK1dtA5EACL3ASDKGKBJePsS.jpg',
-      rating: '7.5',
+        'https://image.tmdb.org/t/p/original/zCEjjb1NH3LLsWeZx47wOeqkezf.jpg',
+      rating: '8.1',
       genres: 'Animation • Sci-Fi & Fantasy • Drama',
     },
   ],
